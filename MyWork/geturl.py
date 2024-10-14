e59a517da7a77e161bdace1060bdcc481c4e3bc7
Francesca Ruberto <francescaruberto01@gmail.com>
2024-10-14 12:34:46 +0100

    first week lab completed

--- MyWork/geturl.py
+++ MyWork/geturl.py
@@ -6,4 +6,10 @@ import requests
 url= "https://api.coindesk.com/v1/bpi/currentprice.json"
 response = requests.get(url)
 data = response.json()
-print(data)
+#print(data)
+
+print(data['bpi']['EUR']['rate_float']) 
+
+# key bpi stands for "Bitcoin Price Index in the data dictionary". 
+# key "EUR" is the exhange currency
+# key "rate_float" contains the numerical value of the exhange, expressed in decimal values. 
